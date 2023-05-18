eece076a1b299038bda6c531f717be46120bec11
Omar H <85137818+Pixlox@users.noreply.github.com>
2023-05-19 00:02:51 +1000

    added cooldown system for commands

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,5 +1,5 @@
 const colours = require('colors');
-const { Events } = require('discord.js');
+const { Events, Collection, EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	name: Events.InteractionCreate,
@@ -7,12 +7,43 @@ module.exports = {
 		if (!interaction.isChatInputCommand()) return;
 
 		const command = interaction.client.commands.get(interaction.commandName);
+		const { cooldowns } = interaction.client;
 
+		if (!cooldowns.has(command.data.name)) {
+			cooldowns.set(command.data.name, new Collection());
+		}
+
+		const now = Date.now();
+		const timestamps = cooldowns.get(command.data.name);
+		const defaultCooldownDuration = 3;
+		const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1000;
+
+		if (timestamps.has(interaction.user.id)) {
+			const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+		
+			if (now < expirationTime) {
+				const expiredTimestamp = Math.round(expirationTime / 1000);
+				// return interaction.reply({ content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`, ephemeral: true });
+				const cooldownEmbed = new EmbedBuilder()
+					.setColor(0x3FA659)
+					.setTitle('On cooldown!')
+					.setDescription(`Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`)
+					.setTimestamp()
+					.setFooter({ text: `Sent by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
+
+
+				return interaction.reply({ embeds: [cooldownEmbed], ephemeral: true });
+			}
+		}
+
+		timestamps.set(interaction.user.id, now);
+		setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+
+				
 		if (!command) {
 			console.error(colours.red('[Electra] [WARNING] ') + `No command matching ${interaction.commandName} was found.`);
 			return;
 		}
-
 		try {
 			await command.execute(interaction);
 		} catch (error) {
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const colours = require('colors');
 colours.enable();
 
+global.instanceCommandCount = 0;
+
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
@@ -9,6 +11,8 @@ const { token } = require('./config.json');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
+client.cooldowns = new Collection();
+
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
@@ -41,3 +45,5 @@ for (const file of eventFiles) {
 
 
 client.login(token);
+
+global.appRoot = path.resolve(__dirname);
