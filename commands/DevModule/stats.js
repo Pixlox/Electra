1b11b855205f347e431cfd0c6b07429b5a26287d
Omar H <85137818+Pixlox@users.noreply.github.com>
2023-05-19 00:05:20 +1000

    added stats command to show instance stats

---
+++ commands/DevModule/stats.js
@@ -0,0 +1,44 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const osu = require('node-os-utils');
+
+const cpu = osu.cpu;
+const formatMemoryUsage = (data) => `${Math.round(data / 1024 / 1024 * 100) / 100} MB`;
+const memoryData = process.memoryUsage();
+const memoryUsage = {
+  rss: `${formatMemoryUsage(memoryData.rss)}`,
+  heapTotal: `${formatMemoryUsage(memoryData.heapTotal)}`,
+  heapUsed: `${formatMemoryUsage(memoryData.heapUsed)}`,
+  external: `${formatMemoryUsage(memoryData.external)}`,
+};
+
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('stats')
+		.setDescription('Provides current stats about the bot.'),
+	async execute(interaction) {
+        global.instanceCommandCount = global.instanceCommandCount + 1;
+
+        cpu.usage().then(cpuPercentage => {
+
+        const statsEmbed = new EmbedBuilder()
+			.setColor(0x3FA659)
+			.setTitle('Instance Statistics')
+            .addFields(
+                { name: 'Version', value: `Alpha ${require(global.appRoot + '/package.json').version}` },
+                { name: 'Author', value: 'Pixlox#1717' },
+                { name: 'Library', value: 'DiscordJS' },
+                { name: 'Server', value: `${interaction.guild.name}` },
+                { name: 'Instance Memory usage', value: memoryUsage.rss },
+                { name: 'Instance CPU usage', value: `${cpuPercentage}%` },
+                { name: 'Commands run on this instance', value: `${global.instanceCommandCount}` },
+            )
+			.setTimestamp()
+			.setFooter({ text: `Sent by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
+
+
+		interaction.reply({ embeds: [statsEmbed] });
+
+        });
+	},
+};
