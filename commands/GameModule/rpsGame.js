b8f312fe4b7cb86da7fbf672fb07595c7d72d12a
Omar H <85137818+Pixlox@users.noreply.github.com>
2023-05-20 00:06:43 +1000

    added rock paper scissors and slap game

---
+++ commands/GameModule/rpsGame.js
@@ -0,0 +1,64 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('rps')
+		.setDescription('Play rock paper scissors against the bot.')
+        .addStringOption(option =>
+            option
+                .setName('option')
+                .setDescription('The option you\'d like to play against the bot.')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'rock', value: 'rock' },
+                    { name: 'paper', value: 'paper' },
+                    { name: 'scissors', value: 'scissors' },
+        )),
+	async execute(interaction) {
+
+        const choices = ['rock', 'paper', 'scissors'];
+        const botChoice = choices[Math.floor(Math.random() * choices.length)];
+        const userChoice = interaction.options.getString('option');
+
+		global.instanceCommandCount = global.instanceCommandCount + 1;
+
+        if (botChoice == interaction.options.getString('option')) {
+            const tieEmbed = new EmbedBuilder()
+                .setColor(0xFFF37C)
+                .setTitle('It\'s a tie!')
+                .setDescription('Game results are placed below.')
+                .addFields({ name: 'I rolled', value: botChoice }, { name: 'You rolled', value: userChoice })
+                .setTimestamp()
+                .setFooter({ text: `Sent by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
+
+            interaction.reply({ embeds: [tieEmbed] });
+            return;
+        }
+
+        if ((botChoice == 'rock' && userChoice == 'paper') || (botChoice == 'paper' && userChoice == 'scissors') || (botChoice == 'scissors' && userChoice == 'rock')) {
+            const userWinEmbed = new EmbedBuilder()
+                .setColor(0x3FA659)
+                .setTitle('You win!')
+                .setDescription('Game results are placed below.')
+                .addFields({ name: 'I rolled', value: botChoice }, { name: 'You rolled', value: userChoice })
+                .setTimestamp()
+                .setFooter({ text: `Sent by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
+
+            interaction.reply({ embeds: [userWinEmbed] });
+            return;
+        }
+
+        if ((botChoice == 'rock' && userChoice == 'scissors') || (botChoice == 'paper' && userChoice == 'rock') || (botChoice == 'scissors' && userChoice == 'paper')) {
+            const userLoseEmbed = new EmbedBuilder()
+                .setColor(0xF95d5d)
+                .setTitle('You lose!')
+                .setDescription('Game results are placed below.')
+                .addFields({ name: 'I rolled', value: botChoice }, { name: 'You rolled', value: userChoice })
+                .setTimestamp()
+                .setFooter({ text: `Sent by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
+
+            interaction.reply({ embeds: [userLoseEmbed] });
+            return;
+        }
+	},
+};
---
+++ commands/GameModule/slapGame.js
@@ -0,0 +1,54 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('slap')
+		.setDescription('Slap a user of your choice. Notifies them if slapped.')
+		.addUserOption(option =>
+            option
+                .setName('user')
+                .setDescription('The user you\'d like to slap.')
+                .setRequired(true),
+        ),	
+
+	async execute(interaction) {
+		global.instanceCommandCount = global.instanceCommandCount + 1;
+		const slapChance = Math.random();
+
+		const slapperName = interaction.user.username;
+		const slapRecieverName = interaction.options.getUser('user').username;
+
+		if (slapChance < 1 / 3) {
+
+			const slapHitEmbed = new EmbedBuilder()
+                .setColor(0x3FA659)
+                .setTitle('The slap hits!')
+                .setDescription(`You slapped ${slapRecieverName}.`)
+                .setTimestamp()
+                .setFooter({ text: `Sent by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
+
+            interaction.reply({ embeds: [slapHitEmbed] });
+
+			const slapHitDMEmbed = new EmbedBuilder()
+                .setColor(0xF95d5d)
+                .setTitle('You just got slapped...')
+                .setDescription(`You got slapped by ${slapperName}.`)
+                .setTimestamp()
+                .setFooter({ text: `Sent by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
+
+
+			interaction.options.getUser('user').send({ embeds: [slapHitDMEmbed] });
+            return;
+
+		} else {
+			const slapMissEmbed = new EmbedBuilder()
+                .setColor(0xF95d5d)
+                .setTitle('The slap misses!')
+                .setDescription('You missed it...somehow.')
+                .setTimestamp()
+                .setFooter({ text: `Sent by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
+
+            interaction.reply({ embeds: [slapMissEmbed] });
+		}
+	},
+};
