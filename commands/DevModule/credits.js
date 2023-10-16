036dec904d3a51daf27f0fa2d603f94a76ce3315
Pixlox <85137818+Pixlox@users.noreply.github.com>
2023-10-16 14:53:40 +1100

    Update

--- commands/DevModule/credits.js
+++ commands/DevModule/credits.js
@@ -13,6 +13,25 @@ module.exports = {
         { name: "Author", value: "Pixlox", inline: true },
         { name: "Main Library", value: "DiscordJS", inline: true },
         { name: "Reddit Wrapper", value: "Snoowrap" },
+        {
+          name: "Auscar",
+          value:
+            "He is ladyboy and loves SUSHANG and Thick tenticle hentai also cursed with Xianzhou characters, talked about Egypt for too long",
+        },
+        {
+          name: "Sika",
+          value:
+            "He loves a certain event that occured on the eleventh of septmeber 2001 and the line 'what rod' reason why maxi hate Egypt",
+        },
+        {
+          name: "Maxi",
+          value:
+            "Hates Egypt and loves looking at SEELE, also have mucho hombre amigos",
+        },
+        {
+          name: "Omar",
+          value: "He loves Silver Wolf, doesn't contribute to Egypt",
+        },
         { name: "Thanks to...", value: `${interaction.user.username}` }
       )
       .setTimestamp()
