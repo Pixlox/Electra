4ed061a604bf35ff854bfbfdcee47c6a0b9a3a68
Pixlox <85137818+Pixlox@users.noreply.github.com>
2023-10-17 21:52:17 +1100

    typo fix

--- commands/GameModule/slapGame.js
+++ commands/GameModule/slapGame.js
@@ -13,63 +13,94 @@ module.exports = {
 
   async execute(interaction) {
     global.instanceCommandCount = global.instanceCommandCount + 1;
-    const slapChance = Math.random();
 
-    const slapperName = interaction.user.username;
-    const slapRecieverName = interaction.options.getUser("user").username;
+    try {
+      const slapChance = Math.random();
+      const slapperName = interaction.user.username;
+      const slapRecieverName = interaction.options.getUser("user").username;
 
-    if (interaction.user.id === interaction.options.getUser("user").id) {
-      const slapSelfEmbed = new EmbedBuilder()
-        .setColor(0xf95d5d)
-        .setTitle("You slapped yourself?")
-        .setDescription("It hurt...")
-        .setTimestamp()
-        .setFooter({
-          text: `Sent by ${interaction.user.username}`,
-          iconURL: interaction.user.displayAvatarURL(),
-        });
+      if (interaction.options.getUser("user").bot == true) {
+        const slapMissEmbed = new EmbedBuilder()
+          .setColor(0xf95d5d)
+          .setTitle("You can't slap a bot.")
+          .setTimestamp()
+          .setFooter({
+            text: `Sent by ${interaction.user.username}`,
+            iconURL: interaction.user.displayAvatarURL(),
+          });
 
-      interaction.editReply({ embeds: [slapSelfEmbed] });
-      return;
-    }
+        interaction.editReply({ embeds: [slapMissEmbed] });
+        return;
+      }
 
-    if (slapChance < 1 / 3 || interaction.user.id === "465672129082556427") {
-      const slapHitEmbed = new EmbedBuilder()
-        .setColor(0x3fa659)
-        .setTitle("The slap hits!")
-        .setDescription(`You slapped ${slapRecieverName}.`)
-        .setTimestamp()
-        .setFooter({
-          text: `Sent by ${interaction.user.username}`,
-          iconURL: interaction.user.displayAvatarURL(),
-        });
+      if (interaction.user.id === interaction.options.getUser("user").id) {
+        const slapSelfEmbed = new EmbedBuilder()
+          .setColor(0xf95d5d)
+          .setTitle("You slapped yourself?")
+          .setDescription("It hurt...")
+          .setTimestamp()
+          .setFooter({
+            text: `Sent by ${interaction.user.username}`,
+            iconURL: interaction.user.displayAvatarURL(),
+          });
 
-      interaction.editReply({ embeds: [slapHitEmbed] });
+        interaction.editReply({ embeds: [slapSelfEmbed] });
+        return;
+      }
 
-      const slapHitDMEmbed = new EmbedBuilder()
-        .setColor(0xf95d5d)
-        .setTitle("You just got slapped...")
-        .setDescription(`You got slapped by ${slapperName}.`)
-        .setTimestamp()
-        .setFooter({
-          text: `Sent by ${interaction.user.username}`,
-          iconURL: interaction.user.displayAvatarURL(),
-        });
+      if (slapChance < 1 / 3 || interaction.user.id === "465672129082556427") {
+        const slapHitEmbed = new EmbedBuilder()
+          .setColor(0x3fa659)
+          .setTitle("The slap hits!")
+          .setDescription(`You slapped ${slapRecieverName}.`)
+          .setTimestamp()
+          .setFooter({
+            text: `Sent by ${interaction.user.username}`,
+            iconURL: interaction.user.displayAvatarURL(),
+          });
 
-      interaction.options.getUser("user").send({ embeds: [slapHitDMEmbed] });
-      return;
-    } else {
-      const slapMissEmbed = new EmbedBuilder()
-        .setColor(0xf95d5d)
-        .setTitle("The slap misses!")
-        .setDescription("You missed it...somehow.")
-        .setTimestamp()
-        .setFooter({
-          text: `Sent by ${interaction.user.username}`,
-          iconURL: interaction.user.displayAvatarURL(),
-        });
+        interaction.editReply({ embeds: [slapHitEmbed] });
 
-      interaction.editReply({ embeds: [slapMissEmbed] });
+        const slapHitDMEmbed = new EmbedBuilder()
+          .setColor(0xf95d5d)
+          .setTitle("You just got slapped...")
+          .setDescription(`You got slapped by ${slapperName}.`)
+          .setTimestamp()
+          .setFooter({
+            text: `Sent by ${interaction.user.username}`,
+            iconURL: interaction.user.displayAvatarURL(),
+          });
+
+        interaction.options.getUser("user").send({ embeds: [slapHitDMEmbed] });
+        return;
+      } else {
+        const slapMissEmbed = new EmbedBuilder()
+          .setColor(0xf95d5d)
+          .setTitle("The slap misses!")
+          .setDescription("You missed it...somehow.")
+          .setTimestamp()
+          .setFooter({
+            text: `Sent by ${interaction.user.username}`,
+            iconURL: interaction.user.displayAvatarURL(),
+          });
+
+        interaction.editReply({ embeds: [slapMissEmbed] });
+      }
+    } catch {
+      if (interaction.options.getUser("user").bot == true) {
+        const slapMissEmbed = new EmbedBuilder()
+          .setColor(0xf95d5d)
+          .setTitle("That user is immune, somehow...")
+          .setDescription("Slap them when you see them I guess.")
+          .setTimestamp()
+          .setFooter({
+            text: `Sent by ${interaction.user.username}`,
+            iconURL: interaction.user.displayAvatarURL(),
+          });
+
+        interaction.editReply({ embeds: [slapMissEmbed] });
+        return;
+      }
     }
   },
 };
