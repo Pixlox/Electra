2b1fd434a8111f1c39aef46001340a1256b1c3b9
Pixlox <85137818+Pixlox@users.noreply.github.com>
2023-10-20 20:38:42 +1100

    updated run.sh script

--- run.sh
+++ run.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-echo "Pulling changes from git..."
+echo "Killing all Node.js instances..."
+pkill -f "node"
 
+echo "Pulling changes from git..."
 git pull
 
 echo "Attempting to install new dependencies..."
-
 npm install
 
 echo "Refreshing application commands..."
-
 node deploy.js
 
 echo "Deploying bot..."
-
 node index.js
